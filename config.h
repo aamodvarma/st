0d7b0b878283b9caaaf290dd34757038b01c3f28
Aamod Varma <aamodvarma@gmail.com>
2021-07-07 13:07:51 +0400

    Changed font

--- config.h
+++ config.h
@@ -5,8 +5,9 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "FiraCode Nerd Font Mono:style=Medium:pixelsize=12:antialias=true:autohint=true";
-static int borderpx = 0;
+static char *font = "mono:pixelsize=12:antialias=true:autohint=true";
+static char *font2[] = { "JoyPixels:pixelsize=10:antialias=true:autohint=true" };
+static int borderpx = 2;
 
 /*
  * What program is execed by st depends of these precedence rules:
